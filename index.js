abbe83664a768efbf5dc7b23b697f13db383e4bd
Eric Desjardins <eric.desjardins11@gmail.com>
2017-03-13 21:52:33 -0300

    Clean up

--- index.js
+++ index.js
@@ -3,26 +3,19 @@ let request = require("request");
 
 let app = express();
 
-
 app.get('/', function(req, res) {
     res.send("hg Mozilla relay service");
 });
 
 app.get('/mozilla_dxr/', function(req, res) {
-
     let hgBranch = req.query.branch;
     let hgRevision = req.query.revision;
     let hgPath = req.query.path;
     let uri = `https://hg.mozilla.org/${hgBranch}/raw-file/${hgRevision}/${hgPath}`;
 
-    // https://hg.mozilla.org/mozilla-central/raw-file/3154da520af5/browser/base/content/test/alerts/browser_notification_close.js
-    //mozilla_dxr?branch=mozilla-central&revision=3154da520af5&path=browser/base/content/test/alerts/browser_notification_close.js
-
+    // mozilla_dxr?branch={BRANCH}&revision={REVSION}&path={path}
     request(uri, function(error, response, body) {
         res.send(body);
     });
-
-    // res.send("tagId is set to " + req.query.tagId);
 })
-
 app.listen(process.env.PORT || 5000);
